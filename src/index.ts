4176e88fa716cf58524197c990e13d3603910c37
kwokpia <kwokpia@gmail.com>
2025-05-08 15:11:11 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 export default {
-	async fetch(request): Promise<Response> {
-		const data = {
-			hello: "world",
-		};
+  async fetch(request) {
+    const url = new URL(request.url);
 
-		return Response.json(data);
-	},
-} satisfies ExportedHandler;
+    switch (url.pathname) {
+      case '/hello':
+        return new Response('hello', {
+          status: 200,
+          headers: { 'Content-Type': 'text/plain' },
+        });
+      case '/world':
+        return new Response('world', {
+          status: 200,
+          headers: { 'Content-Type': 'text/plain' },
+        });
+      default:
+        return new Response('Not Found', {
+          status: 404,
+          headers: { 'Content-Type': 'text/plain' },
+        });
+    }
+  },
+};
